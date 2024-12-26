b9f39c68a9c75d429d4299f7995c6fcfc3fb6bb4
Nonprawich I <40120130+npwitk@users.noreply.github.com>
2024-12-26 20:07:05 +0700

    Initial Commit

---
+++ Name That Bop/Name_That_BopApp.swift
@@ -0,0 +1,17 @@
+//
+//  Name_That_BopApp.swift
+//  Name That Bop
+//
+//  Created by Nonprawich I. on 26/12/2024.
+//
+
+import SwiftUI
+
+@main
+struct Name_That_BopApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
